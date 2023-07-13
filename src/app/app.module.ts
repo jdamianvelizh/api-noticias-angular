2208f27c56214aba11b1834dbf0909fb3eb7d44c
DamianVH1 <josephveliz99@hotmail.com>
2023-07-12 19:39:14 -0600

    Commit inicial - creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { ListadoNoticiasComponent } from './components/listado-noticias/listado-noticias.component';
+import { NavabarComponent } from './components/navabar/navabar.component';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent,
+    ListadoNoticiasComponent,
+    NavabarComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule
