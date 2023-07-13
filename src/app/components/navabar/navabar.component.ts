712e1a0f707f261439b4d79befd7c41ae291f63f
DamianVH1 <josephveliz99@hotmail.com>
2023-07-12 20:05:40 -0600

    creacion del navbar

--- src/app/components/navabar/navabar.component.ts
+++ src/app/components/navabar/navabar.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navabar',
   templateUrl: './navabar.component.html',
   styleUrls: ['./navabar.component.css']
 })
-export class NavabarComponent {
+export class NavabarComponent  implements OnInit {
+  
+  @Input() titulo:string;
+
+
+  constructor(){
+    this.titulo = ''
+  }
+  
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
